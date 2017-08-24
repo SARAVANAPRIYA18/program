facd9279bc45b885949685ad341a1c602a066662
SARAVANAPRIYA18 <30409200+SARAVANAPRIYA18@users.noreply.github.com>
2017-08-24 12:05:39 +0530

    Update and rename README.md to string.c

---
+++ string.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j,n;
+char str[100];
+char rev[100];
+printf("Enter number:");
+scanf("%s",str[100]);
+for(i=0;str[i]='\0';i++)
+{
+k=i-1;
+}
+for(j=0;j<=i-1;j++)
+{
+rev[j]=str[k];
+k--;
+}
+getch();
+}
+
+
+
+
+
+
+
+
+
+
+,
